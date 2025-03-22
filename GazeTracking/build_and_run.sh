a00293f723177246783cf130ab9827d8a943a0c3
youthboo <ppalita.jk@gmail.com>
2025-03-23 00:47:29 +0700

    hot ka

--- GazeTracking/build_and_run.sh
+++ GazeTracking/build_and_run.sh
@@ -5,9 +5,8 @@ xhost local:root
 # build and run docker image
 ([ "$(docker images -q ${IMAGE_NAME})" == "" ] && docker build -t ${IMAGE_NAME} . ) 
 ([ "$(docker images -q ${IMAGE_NAME})" != "" ] && \
-docker run --rm --device /dev/video0:/dev/video0 \
-  -e DISPLAY=${DISPLAY} \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  --env="QT_X11_NO_MITSHM=1" \
-  -it ${IMAGE_NAME} bash
-)
+docker run --rm --device /dev/video0 \
+	-e DISPLAY=${DISPLAY} \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	--env="QT_X11_NO_MITSHM=1" \
+	-it ${IMAGE_NAME} bash)
